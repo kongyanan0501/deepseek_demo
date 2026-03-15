e2472efd36e51d8452f42621ae0a9f775b02c897
kongyanan0501 <kongyanan0501@gmail.com>
2026-03-15 15:18:08 +0800

    3commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: streamlit run pumpkin_ai_app.py --server.port=$PORT --server.address=0.0.0.0
+web: ./start.sh
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Railway 等平台通过 PORT 环境变量指定端口
-PORT=${PORT:-8080}
-exec streamlit run pumpkin_ai_app.py --server.port=$PORT --server.address=0.0.0.0
+# Railway 通过 PORT 环境变量指定端口，用 STREAMLIT_SERVER_PORT 传给 Streamlit
+export STREAMLIT_SERVER_PORT=${PORT:-8080}
+exec streamlit run pumpkin_ai_app.py --server.address=0.0.0.0
